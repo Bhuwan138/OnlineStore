7c706bc85fbfc47ccb6cb1a4e2111a0001cea452
unknown <pandeybhuwan138@gmail.com>
2023-07-06 14:52:57 +0545

    fixed

--- src/Components/CarouselComponent.jsx
+++ src/Components/CarouselComponent.jsx
@@ -1,33 +1,38 @@
 import Carousel from "react-bootstrap/Carousel";
 import styled from "./CarouselComponent.module.css";
+import c1 from "/public/images/c1.png";
+import c2 from "/public/images/c2.png";
+import c3 from "/public/images/c3.png";
+import c4 from "/public/images/c4.png";
+
 const CarouselComponent = () => {
   return (
     <Carousel>
       <Carousel.Item interval={1000}>
         <img
           className={`d-block w-100 ${styled.heightImg}`}
-          src="/public/images/c1.png"
+          src={c1}
           alt="First slide"
         />
       </Carousel.Item>
       <Carousel.Item interval={1000}>
         <img
           className={`d-block w-100 ${styled.heightImg}`}
-          src="/public/images/c2.png"
+          src={c2}
           alt="Second slide"
         />
       </Carousel.Item>
       <Carousel.Item interval={1000}>
         <img
           className={`d-block w-100 ${styled.heightImg}`}
-          src="/public/images/c3.png"
+          src={c3}
           alt="Third slide"
         />
       </Carousel.Item>
       <Carousel.Item interval={1000}>
         <img
           className={`d-block w-100 ${styled.heightImg}`}
-          src="/public/images/c4.png"
+          src={c4}
           alt="Third slide"
         />
       </Carousel.Item>
